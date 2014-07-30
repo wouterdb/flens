747877c085d919e313c16e97cd2f270ad110d5ed
wouterdb <w.deborger@gmail.com>
2014-07-30 17:23:01 +0200

    completed batch plugin (partly untested)
    fixes #1622

---
+++ src/main/java/flens/config/BatchPlugin.java
@@ -0,0 +1,37 @@
+package flens.config;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import flens.config.util.ActiveFilter;
+import flens.filter.BatchingFilter;
+import flens.input.util.InputQueueExposer;
+import flens.statefull.AlertOutput;
+
+public class BatchPlugin extends ActiveFilter{
+
+	@Override
+	public String getDescription() {
+		return "plugin packing togheter records in super records";
+	}
+
+	@Override
+	protected void construct() {
+		int interval = getInt("interval", 97);
+		int maxbatch = getInt("maxbatch", 100);
+	
+		InputQueueExposer inpex = new InputQueueExposer(name+"_in",plugin, tagger);
+		engine.addInput(inpex);
+		
+		engine.addOutput(new BatchingFilter(name,plugin,matcher,inpex,maxbatch,interval));
+		
+	}
+	
+	public List<Option> getOptions() {
+		List<Option>  out = new LinkedList<>(super.getOptions());
+		out.add(new Option("interval", "int", "97", "maximal time to wait before a bacth is sent in ms"));
+		out.add(new Option("maxbatch", "int", "100", "maximal nr of records per super record"));
+		return out;
+	}
+
+}
---
+++ src/main/java/flens/config/UnBatchPlugin.java
@@ -0,0 +1,65 @@
+/**
+ *
+ *     Copyright 2013 KU Leuven Research and Development - iMinds - Distrinet
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ *     Administrative Contact: dnet-project-office@cs.kuleuven.be
+ *     Technical Contact: wouter.deborger@cs.kuleuven.be
+ */
+package flens.config;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import flens.config.util.AbstractConfig;
+
+public class UnBatchPlugin extends AbstractConfig{
+	
+	@Override
+	public String getDescription() {
+		return "split a batch record into its constituent sub records";
+	}
+
+	
+	@Override
+	protected boolean isIn() {
+		return false;
+	}
+	
+	@Override
+	protected boolean isOut() {
+		return false;
+	}
+
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	protected void construct() {
+		boolean keep = getBool("keep", false);
+		engine.addFilter(new flens.filter.UnBatchingFilter(name,plugin,tagger,matcher,prio,keep));
+
+	}
+
+	
+
+
+	
+	@Override
+	public List<Option> getOptions() {
+		List<Option>  out = new LinkedList<Option>(super.getOptions());
+		out.add(new Option("keep", "Boolean", "false", "keep super record after extraction of sub records"));
+		return out;
+	}
+}
